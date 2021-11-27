8536af3c0c48a0821a7a6565b44635489e9cc85c
tubacatal <tubacatal90@gmail.com>
2021-11-27 22:46:26 +0300

    user creation

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Available Scripts
 ### `npx http-server -c-1 -p 8080 -P http://localhost:3000`
 Run the client on port 8080 to listen port 3000.
+Run command while in "express-tdd/client"
 
 ### `npm start`
 Run the server on port 3000. 
--- source/__tests__/UserRegister.spec.js
+++ source/__tests__/UserRegister.spec.js
@@ -1,5 +1,18 @@
 const request = require("supertest");
 const app = require("../src/app");
+const User = require("../user/User");
+const sequelize = require("../config/database");
+
+// make sure db is created before test are runned
+beforeAll(() => {
+  jest.setTimeout(20000);
+  return sequelize.sync();
+});
+
+// clear the table before each test for predictable behaviour
+beforeEach(() => {
+  return User.destroy({ truncate: true });
+});
 
 describe("User Registration", () => {
   it("returns 200 when signup request is vaild", (done) => {
@@ -7,7 +20,7 @@ describe("User Registration", () => {
       .post("/api/1.0/users")
       .send({
         username: "user1",
-        email: "user1.email.com",
+        email: "user1@email.com",
         password: "P4ssword",
       })
       .then((response) => {
@@ -21,12 +34,47 @@ describe("User Registration", () => {
       .post("/api/1.0/users")
       .send({
         username: "user1",
-        email: "user1.email.com",
+        email: "user1@email.com",
         password: "P4ssword",
       })
       .then((response) => {
         expect(response.body.message).toBe("User created");
         done();
+      });
+  });
+
+  it("saves user to database", (done) => {
+    request(app)
+      .post("/api/1.0/users")
+      .send({
+        username: "user1",
+        email: "user1@email.com",
+        password: "P4ssword",
+      })
+      .then(() => {
+        // check the database if instance exists
+        User.findAll().then((userList) => {
+          expect(userList.length).toBe(1);
+          done();
+        });
+      });
+  });
+
+  it("saves the username and email to database", (done) => {
+    request(app)
+      .post("/api/1.0/users")
+      .send({
+        username: "user1",
+        email: "user1@email.com",
+        password: "P4ssword",
+      })
+      .then(() => {
+        User.findAll().then((userList) => {
+          const savedUser = userList[0];
+          expect(savedUser.username).toBe("user1");
+          expect(savedUser.email).toBe("user1@email.com");
+          done();
+        });
       });
   });
 });
