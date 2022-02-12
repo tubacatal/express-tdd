3cd4da663bf4f0245e065da37de26a297511f175
tubacatal <tubacatal90@gmail.com>
2022-02-12 22:57:15 +0300

    configurations for different envs

--- source/src/config/database.js
+++ source/src/config/database.js
@@ -1,9 +1,12 @@
 const Sequelize = require('sequelize');
+const config = require('config');
 
-const sequelizeInstance = new Sequelize('hoaxify', 'my-db-user', 'db-p4ss', {
-  dialect: 'sqlite',
-  storage: '../database.sqlite',
-  logging: false,
+const dbConfig = config.get('database');
+
+const sequelizeInstance = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+  dialect: dbConfig.dialect,
+  storage: dbConfig.storage,
+  logging: dbConfig.logging,
 });
 
 module.exports = sequelizeInstance;
